27f7ae7ae33e1fd7968cc2d6e819368ae51a7632
Josef Richter <josef.richter@me.com>
2020-06-15 18:51:13 +0200

    Initial Commit

---
+++ NotesTaker/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  NotesTaker
+//
+//  Created by JOSEF RICHTER on 15/06/2020.
+//  Copyright © 2020 JOSEF RICHTER. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
