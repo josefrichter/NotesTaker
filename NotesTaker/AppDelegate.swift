c6a818eed3a702e1621a9f5ca6b950d676dfa9e0
Mark Cornelisse <m.p.cornelisse@gmail.com>
2020-06-23 16:38:46 +0200

    - Fixed the focus to the popover problem.

--- NotesTaker/AppDelegate.swift
+++ NotesTaker/AppDelegate.swift
@@ -11,15 +11,15 @@ import SwiftUI
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     var popover: NSPopover!
     var statusBarItem: NSStatusItem!
-
-
+    
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
-
+        
         // Create the popover
         let popover = NSPopover()
         popover.contentSize = NSSize(width: 400, height: 500)
@@ -30,25 +30,26 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
         if let button = self.statusBarItem.button {
             button.title = "Note"
-//             button.image = NSImage(named: "Icon")
-             button.action = #selector(togglePopover(_:))
+            //             button.image = NSImage(named: "Icon")
+            button.action = #selector(togglePopover(_:))
         }
     }
     
     @objc func togglePopover(_ sender: AnyObject?) {
-         if let button = self.statusBarItem.button {
-              if self.popover.isShown {
-                   self.popover.performClose(sender)
-              } else {
-                   self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
-              }
-         }
+        if let button = self.statusBarItem.button {
+            if self.popover.isShown {
+                self.popover.performClose(sender)
+            } else {
+                NSApp.activate(ignoringOtherApps: true)
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            }
+        }
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    
 }
 
